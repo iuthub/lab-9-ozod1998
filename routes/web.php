c5325e6d27645e143e68f761952e6b55237d5de3
ozod1998 <33785422+ozod1998@users.noreply.github.com>
2019-04-28 14:54:23 +0500

    Lab 9 finished by U1710010
    Tagoev Ozod 
    U1710010
    CSE -17-1
    Section 001

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,44 +9,44 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
 Route::get('/', [
     'uses' => 'PostController@getIndex',
     'as' => 'blog.index'
 ]);
-
 Route::get('post/{id}', [
     'uses' => 'PostController@getPost',
     'as' => 'blog.post'
 ]);
-
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
-
+Route::get('like/{id}', [
+    'uses' => 'PostController@getLikePost',
+    'as' => 'blog.post.like'
+]);
 Route::group(['prefix' => 'admin'], function() {
     Route::get('', [
         'uses' => 'PostController@getAdminIndex',
         'as' => 'admin.index'
     ]);
-
     Route::get('create', [
         'uses' => 'PostController@getAdminCreate',
         'as' => 'admin.create'
     ]);
-
     Route::post('create', [
         'uses' => 'PostController@postAdminCreate',
         'as' => 'admin.create'
     ]);
-
     Route::get('edit/{id}', [
         'uses' => 'PostController@getAdminEdit',
         'as' => 'admin.edit'
     ]);
-
     Route::post('edit', [
         'uses' => 'PostController@postAdminUpdate',
         'as' => 'admin.update'
+    ]);
+    Route::get('delete/{id}', [
+        'uses' => 'PostController@getAdminDelete',
+        'as' => 'admin.delete'
     ]);
 });
